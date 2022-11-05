086774ef5a7eb98568519b00b08d797831e82bb7
e94116075 <e94116075@gs.ncku.edu.tw>
2022-11-05 18:21:19 +0000

    first commit

---
+++ lab08/lab08.py
@@ -0,0 +1,18 @@
+import os as o 
+n=o.getcwd()
+list1=n.split(o.sep)
+print(type(list1))
+list1.remove('')
+print(list1)
+print(o.listdir('/home/E94116075/'))
+list2=list(o.listdir('/home/E94116075/'))
+path='E94116075.txt'
+f=open(path,'w')
+for m in list1:
+    f.writelines(m+o.linesep)
+f.writelines('\n')
+for m in list2:
+    f.writelines(m+o.linesep)
+f.close()
+
+
