cfd923fb2bb8bedfdee1f932aa7cdd537e894ad3
e94116075 <e94116075@gs.ncku.edu.tw>
2022-12-12 06:49:44 +0000

    first commit

---
+++ lab12/lab12.py
@@ -0,0 +1,35 @@
+import pymysql
+import requests
+r = requests.get('https://citypark.tainan.gov.tw/App/parking.ashx?verCode=5177E3481D&type=1&ftype=1&exportTo=2')
+list1=r.json()
+types=[]
+nums=[]
+names=[]
+address=[]
+for i in range(0,10):
+    dict0=list1[i]
+    types.append(dict0['停車場型態'])
+    nums.append(dict0['停車場代碼'])
+    names.append(dict0['停車場名稱'])
+    address.append(dict0['停車場地址'])
+print(types)
+# 資料庫設定
+db_settings = {
+    "host": "localhost",
+    "port": 3306,
+    "user": "E94116075",
+    "password": "921123Aa",
+    "db": "wordpress",
+    "charset": "utf8"
+}
+conn = pymysql.connect(**db_settings)
+    # 建立Cursor物件
+with conn.cursor() as cursor:
+        # 新增資料SQL語法
+    command = "INSERT INTO `臺南市公有免費停車場`(`停車場型態`,`停車場代碼`,`停車場名稱`,`停車場地址`)VALUES(%s, %s, %s,%s)"
+    for i in range(0,10) :
+        cursor.execute(
+            command, (types[i], nums[i],names[i],address[i]))
+        # 儲存變更
+    conn.commit()
+conn.close()
