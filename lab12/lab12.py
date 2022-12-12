a1f50045f5aaa694e0d3acc5cde793fc2df90840
e94116075 <113426779+e94116075@users.noreply.github.com>
2022-12-12 14:53:32 +0800

    Update lab12.py

--- lab12/lab12.py
+++ lab12/lab12.py
@@ -1,6 +1,6 @@
 import pymysql
 import requests
-r = requests.get('https://citypark.tainan.gov.tw/App/parking.ashx?verCode=5177E3481D&type=1&ftype=1&exportTo=2')
+r = requests.get('https://citypark.tainan.gov.tw/App/parking.ashx?verCode=5177E3481D&type=1&ftype=1&exportTo=2')#得到這個網址的資料
 list1=r.json()
 types=[]
 nums=[]
@@ -12,9 +12,8 @@ for i in range(0,10):
     nums.append(dict0['停車場代碼'])
     names.append(dict0['停車場名稱'])
     address.append(dict0['停車場地址'])
-print(types)
-# 資料庫設定
-db_settings = {
+
+db_settings = {# 資料庫設定
     "host": "localhost",
     "port": 3306,
     "user": "E94116075",
@@ -22,14 +21,14 @@ db_settings = {
     "db": "wordpress",
     "charset": "utf8"
 }
-conn = pymysql.connect(**db_settings)
-    # 建立Cursor物件
-with conn.cursor() as cursor:
-        # 新增資料SQL語法
-    command = "INSERT INTO `臺南市公有免費停車場`(`停車場型態`,`停車場代碼`,`停車場名稱`,`停車場地址`)VALUES(%s, %s, %s,%s)"
+conn = pymysql.connect(**db_settings)#連線
+    
+with conn.cursor() as cursor:# 建立Cursor物件
+        
+    command = "INSERT INTO `臺南市公有免費停車場`(`停車場型態`,`停車場代碼`,`停車場名稱`,`停車場地址`)VALUES(%s, %s, %s,%s)"# 新增資料SQL語法
     for i in range(0,10) :
         cursor.execute(
             command, (types[i], nums[i],names[i],address[i]))
-        # 儲存變更
-    conn.commit()
-conn.close()
+        
+    conn.commit()# 儲存變更
+conn.close()#關閉
